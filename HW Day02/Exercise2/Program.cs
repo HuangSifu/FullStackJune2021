566beefd5249001c27c978e9b67664e1331147ae
Bochen Huang <80193213+HuangSifu@users.noreply.github.com>
2021-06-10 16:46:09 -0400

    Add files via upload

---
+++ HW Day02/Exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length of the array: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[length];
+            Console.WriteLine("Please enter values of the array: ");
+            for (int i = 0; i < length; i++)
+            {
+                A[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Solution solution = new Solution();
+
+            Console.WriteLine(solution.solution(A));
+        }
+    }
+}
---
+++ HW Day02/Exercise4/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise4
+{
+    class Solution
+    {
+        public void Take2DToSpiral(int[,]A)
+        {
+            int row_start = 0, col_start = 0;
+            int row_end = A.GetLength(0);
+            int col_end = A.GetLength(1);
+            int iterator;
+
+            while(row_start < row_end && col_start < col_end)
+            {
+                for(iterator = col_start; iterator < col_end; ++iterator)
+                {
+                    Console.Write(A[row_start, iterator] + " ");
+                }
+                row_start++;
+
+                for(iterator = row_start; iterator < row_end; ++iterator)
+                {
+                    Console.Write(A[iterator, col_end - 1] + " ");
+                }
+                col_end--;
+
+                if(row_start < row_end)
+                {
+                    for(iterator = col_end - 1; iterator >= col_start; --iterator)
+                    {
+                        Console.Write(A[row_end - 1, iterator] + " ");
+                    }
+                    row_end--;
+                }
+
+                if(col_start < col_end)
+                {
+                    for (iterator = row_end - 1; iterator >= row_start; --iterator)
+                    {
+                        Console.Write(A[iterator, col_start] + " ");
+                    }
+                    col_start++;
+                }
+            }
+        }
+    }
+}
